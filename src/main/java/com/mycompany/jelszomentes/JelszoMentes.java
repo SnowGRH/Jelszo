004784421d4ea714b159d5cebc0bead0ed699d24
Vinczeffy Gergely (SZF_2022_2024) <vinczeffy.gergely@diak.szamalk-szalezi.hu>
2023-09-13 13:23:30 +0200

    Alap kész, eset 3-ig

---
+++ src/main/java/com/mycompany/jelszomentes/JelszoMentes.java
@@ -0,0 +1,46 @@
+package com.mycompany.jelszomentes;
+
+import java.util.Scanner;
+
+public class JelszoMentes {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Ellenrőzés();
+    }
+
+    private static int Bekeres() {
+        System.out.print("Kód megadása: ");
+        int Ujkod = sc.nextInt();
+        while (Integer.toString(Ujkod).length() < 4 || Integer.toString(Ujkod).length() > 6) {
+            System.out.println("Kód 4 és 6 karakter között kell lenni : ");
+            Ujkod = sc.nextInt();
+        }
+        System.out.println("Jelszó elmnetve");
+        return Ujkod;
+
+    }
+
+    private static void Ellenrőzés() {
+        int kod = Bekeres();
+        int i = 1;
+        String txt = "Hibás pin kód!";
+        System.out.printf("Kérem a belépési kódot(%s/3)", i);
+        int Ekod = sc.nextInt();
+        System.out.println(txt);
+        while (Integer.toString(kod).length() != Integer.toString(Ekod).length() && 3 > i) {
+            i++;
+            System.out.printf("Kérem a belépési kódot(%s/3)", i);
+            Ekod = sc.nextInt();
+            System.out.println(txt);
+        }
+        if (i == 3) {
+            System.out.println("Belépés megtagadva!");
+        } else {
+            System.out.println("Rendben, sikeres belépés!");
+        }
+
+    }
+
+}
